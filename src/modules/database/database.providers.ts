c88984b4323cf5d45d2b426abf627e60554c3150
Anthony Lopez Vega <anthony.lopez@administradors-iMac.local>
2019-04-25 10:28:48 -0500

    fix uri db

--- src/modules/database/database.providers.ts
+++ src/modules/database/database.providers.ts
@@ -5,7 +5,7 @@ export const databaseProviders = [
   {
     provide: 'DbConnectionToken',
     useFactory: async(): Promise<typeof mongoose> => 
-    await mongoose.connect('mongodb://localhost/nestservice', {
+    await mongoose.connect('mongodb://admin:admin2019@ds147436.mlab.com:47436/db-user-service', {
       useNewUrlParser: true,
     })
   }
