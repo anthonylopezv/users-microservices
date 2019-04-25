4b9e3571db35142b6933683f568410dfa770a8a9
Anthony Lopez Vega <anthony.lopez@administradors-iMac.local>
2019-04-25 11:09:07 -0500

    fix host tcp ok

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   app.connectMicroservice({
     transport: Transport.TCP,
     options: {
-      host: 'localhost',
+      host: '157.230.212.28',
       port: 1337,
       retryAttempts: 5,
       retryDelay: 3000,
