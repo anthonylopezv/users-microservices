ff7d12c7e3b7c452c76a922062c5661bca8910be
Anthony Lopez Vega <anthony.lopez@administradors-iMac.local>
2019-04-26 12:00:31 -0500

    fix main.ts

--- src/main.ts
+++ src/main.ts
@@ -11,13 +11,13 @@ async function bootstrap() {
   app.connectMicroservice({
     transport: Transport.TCP,
     options: {
-      host: '157.230.212.28',
-      port: 1337,
+      host: String(process.env.TCP_HOST),
+      port: parseInt(process.env.TCP_PORT),
       retryAttempts: 5,
       retryDelay: 3000,
     }
   })
   await app.startAllMicroservicesAsync();
-  await app.listen(process.env.PORT);
+  await app.listen(parseInt(process.env.PORT));
 }
 bootstrap();
